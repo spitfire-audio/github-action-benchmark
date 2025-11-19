447bbce759e5ce83dbcb60c82d095b3b61c52b55
Matt Zapp <matt.zapp@splice.com>
2025-11-19 14:03:44 -0500

    really trying to get this deployment working

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -4,32 +4,6 @@ set -e
 
 version="$1"
 
-if [[ "$version" == "" ]]; then
-    echo 'Release branch name must be given as first argument' >&2
-    exit 1
-fi
-
-if [ ! -d .git ]; then
-    echo 'This script must be run at root directory of this repository' >&2
-    exit 1
-fi
-
-if ! git diff --quiet; then
-    echo 'Working tree is dirty! Please ensure all changes are committed and working tree is clean' >&2
-    exit 1
-fi
-
-if ! git diff --cached --quiet; then
-    echo 'Git index is dirty! Please ensure all changes are committed and Git index is clean' >&2
-    exit 1
-fi
-
-branch="$(git symbolic-ref --short HEAD)"
-if [[ "$branch" != "master" ]]; then
-    echo 'Current branch is not master. Please move to master before running this script' >&2
-    exit 1
-fi
-
 echo "Releasing to $version branch..."
 
 rm -rf dist
@@ -49,9 +23,7 @@ rsync -R -v dist/src/*.js .release/
 rsync -R -v dist/src/**/*.js .release/
 cp -R node_modules .release/node_modules
 
-git checkout "$version"
-git pull
-git rm -rf node_modules
+git checkout -b "$version"
 rm -rf node_modules  # remove node_modules/.cache
 
 rm -rf dist
